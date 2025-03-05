9d8878504e40081673f19b265f89aca0666b623e
joshua122388 <82735801+joshua122388@users.noreply.github.com>
2025-03-05 16:26:17 -0600

    cambios
    nuevos cambios

---
+++ src/presentacion/test_Conexion.java
@@ -0,0 +1,9 @@
+package presentacion;
+
+import accesoDatos.ConexionSQL; // se importa la capa de acceso a datos que realiza la conexion a la base de datos
+
+public class test_Conexion {
+    public static void main(String[] args) { // El código debe ir dentro del método main
+        ConexionSQL.conectar(); // Llamada correcta al método estático
+    }
+}
