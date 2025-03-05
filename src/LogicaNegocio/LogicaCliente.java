a2ee8a3d95f8f4946a1d93d55612e46e6e4635de
joshua122388 <82735801+joshua122388@users.noreply.github.com>
2025-03-05 17:18:49 -0600

    new code

---
+++ src/LogicaNegocio/LogicaCliente.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LogicaNegocio;
+import Servicios.Cliente_Servicios;
+import entidades.Cliente;
+
+/**
+ *
+ * @author contr
+ */
+public class LogicaCliente implements Cliente_Servicios{
+
+    @Override
+    public void registrarCliente(Cliente cliente) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+}
